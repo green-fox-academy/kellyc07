2317e0d25c57aed28457c3fe67d2f1fb89bcd823
kelly <1710659147@qq.com>
2018-11-08 17:56:56 +0800

    day4
    day4 tasks

---
+++ repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet you instead of the World!
+            Console.WriteLine("Hello, me!");
+        }
+    }
+}
---
+++ repos/firsttestcl/firsttestcl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firsttestcl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,joe");
+            Console.WriteLine("hello,kelly");
+            Console.WriteLine("hello,belly");
+       
+        }
+    }
+}
